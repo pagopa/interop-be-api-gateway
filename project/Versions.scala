42942fa8456dad48dd07087ac42b0640b2db765a
P3trur0 <ubaldopescatore@gmail.com>
2022-02-18 16:33:58 +0100

    missing config params added to application-standalone

--- project/Versions.scala
+++ project/Versions.scala
@@ -21,6 +21,6 @@ object PDNDVersions {
   lazy val authorizationManagementVersion     = "0.1.1-SNAPSHOT"
   lazy val catalogManagementVersion           = "0.1.1-SNAPSHOT"
   lazy val commonsVersion                     = "0.1.1-SNAPSHOT"
-  lazy val partyManagementVersion             = "0.1.3-SNAPSHOT"
+  lazy val partyManagementVersion             = "0.1.5-SNAPSHOT"
   lazy val purposeManagementVersion           = "0.1.1-SNAPSHOT"
 }
