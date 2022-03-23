86251e24bfb99dac076a51c3cc281458dd7ec2f5
Scala Steward <scala-steward@pagopa.it>
2022-03-23 16:06:09 +0000

    Update akka-http, akka-http-spray-json, ... to 10.2.9 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,18 +1,18 @@
 object Versions {
   lazy val akkaHttpJson4sVersion    = "1.38.2"
-  lazy val akkaHttpVersion          = "10.2.6"
+  lazy val akkaHttpVersion          = "10.2.9"
   lazy val akkaManagementVersion    = "1.1.1"
   lazy val akkaPersistenceS3Version = "1.0.16"
   lazy val akkaVersion              = "2.6.17"
   lazy val catsVersion              = "2.7.0"
-  lazy val jacksonVersion           = "2.11.4" // This cannot be updated yet because akka-serialization use 2.11.x version
-  lazy val json4sVersion            = "4.0.4"
-  lazy val kamonVersion             = "2.5.0"
-  lazy val logbackVersion           = "1.2.11"
-  lazy val mockitoVersion           = "3.9.0"
-  lazy val mustacheVersion          = "0.9.10"
-  lazy val scalaMockVersion         = "5.2.0"
-  lazy val scalatestVersion         = "3.2.11"
+  lazy val jacksonVersion   = "2.11.4" // This cannot be updated yet because akka-serialization use 2.11.x version
+  lazy val json4sVersion    = "4.0.4"
+  lazy val kamonVersion     = "2.5.0"
+  lazy val logbackVersion   = "1.2.11"
+  lazy val mockitoVersion   = "3.9.0"
+  lazy val mustacheVersion  = "0.9.10"
+  lazy val scalaMockVersion = "5.2.0"
+  lazy val scalatestVersion = "3.2.11"
 }
 
 object PagopaVersions {
