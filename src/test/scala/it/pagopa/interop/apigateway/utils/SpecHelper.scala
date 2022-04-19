341c271d16597fcfe183e114b81d30bcf3cc27d3
galales <25105748+galales@users.noreply.github.com>
2022-04-19 11:05:29 +0200

    pin-1264: Code cleaning

--- src/test/scala/it/pagopa/interop/apigateway/utils/SpecHelper.scala
+++ src/test/scala/it/pagopa/interop/apigateway/utils/SpecHelper.scala
@@ -1,28 +1,18 @@
 package it.pagopa.interop.apigateway.utils
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import com.nimbusds.jwt.JWTClaimsSet
-import com.typesafe.config.{Config, ConfigFactory}
 import it.pagopa.interop.apigateway.api.impl.GatewayApiServiceImpl
 import it.pagopa.interop.apigateway.service._
 import it.pagopa.interop.authorizationmanagement.client.{model => AuthorizationManagement}
 import it.pagopa.interop.catalogmanagement.client.{model => CatalogManagement}
-import it.pagopa.interop.commons.utils.service.{OffsetDateTimeSupplier, UUIDSupplier}
 import org.scalamock.handlers.CallHandler2
 import org.scalamock.scalatest.MockFactory
 import spray.json.DefaultJsonProtocol
 
 import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Success, Try}
 
 trait SpecHelper extends SprayJsonSupport with DefaultJsonProtocol with MockFactory {
-
-  final val bearerToken: String = "token"
-
-  val config: Config = ConfigFactory
-    .parseResourcesAnySyntax("application-test")
-    .resolve()
 
   val mockAgreementManagementService: AgreementManagementService                 = mock[AgreementManagementService]
   val mockAuthorizationManagementService: AuthorizationManagementService         = mock[AuthorizationManagementService]
@@ -31,9 +21,6 @@ trait SpecHelper extends SprayJsonSupport with DefaultJsonProtocol with MockFact
   val mockCatalogManagementService: CatalogManagementService                     = mock[CatalogManagementService]
   val mockAttributeRegistryManagementService: AttributeRegistryManagementService =
     mock[AttributeRegistryManagementService]
-
-  val mockUUIDSupplier: UUIDSupplier               = mock[UUIDSupplier]
-  val mockDateTimeSupplier: OffsetDateTimeSupplier = mock[OffsetDateTimeSupplier]
 
   val service: GatewayApiServiceImpl =
     GatewayApiServiceImpl(
@@ -44,8 +31,6 @@ trait SpecHelper extends SprayJsonSupport with DefaultJsonProtocol with MockFact
       mockAttributeRegistryManagementService,
       mockPurposeManagementService
     )(ExecutionContext.global)
-
-  def mockSubject(uuid: String): Try[JWTClaimsSet] = Success(new JWTClaimsSet.Builder().subject(uuid).build())
 
   def mockClientRetrieve(clientId: UUID, result: AuthorizationManagement.Client)(implicit
     contexts: Seq[(String, String)]
