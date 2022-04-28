04b4a28a3869ef8f76b540764f4cfa6454844ce0
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-04-28 09:40:14 +0200

    pin-1382: Comma separated strings for accepted audiences (#47)

--- src/main/scala/it/pagopa/interop/apigateway/common/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/apigateway/common/ApplicationConfiguration.scala
@@ -1,20 +1,22 @@
 package it.pagopa.interop.apigateway.common
 
 import com.typesafe.config.{Config, ConfigFactory}
-import scala.jdk.CollectionConverters.ListHasAsScala
 
 object ApplicationConfiguration {
 
-  lazy val config: Config = ConfigFactory.load()
+  val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("interop-api-gateway.port")
+  val serverPort: Int = config.getInt("interop-api-gateway.port")
 
-  lazy val agreementManagementURL: String         = config.getString("services.agreement-management")
-  lazy val authorizationManagementURL: String     = config.getString("services.authorization-management")
-  lazy val catalogManagementURL: String           = config.getString("services.catalog-management")
-  lazy val partyManagementURL: String             = config.getString("services.party-management")
-  lazy val attributeRegistryManagementURL: String = config.getString("services.attribute-registry-management")
-  lazy val purposeManagementURL: String           = config.getString("services.purpose-management")
+  val agreementManagementURL: String         = config.getString("services.agreement-management")
+  val authorizationManagementURL: String     = config.getString("services.authorization-management")
+  val catalogManagementURL: String           = config.getString("services.catalog-management")
+  val partyManagementURL: String             = config.getString("services.party-management")
+  val attributeRegistryManagementURL: String = config.getString("services.attribute-registry-management")
+  val purposeManagementURL: String           = config.getString("services.purpose-management")
 
-  lazy val interopAudience: Set[String] = config.getStringList("interop-api-gateway.jwt.audience").asScala.toSet
+  val interopAudience: Set[String] =
+    config.getString("interop-api-gateway.jwt.audience").split(",").toSet.filter(_.nonEmpty)
+
+  require(interopAudience.nonEmpty, "Audience cannot be empty")
 }
