881aaae0172de1e500b661d2db252026eeaf27d3
galales <25105748+galales@users.noreply.github.com>
2022-09-16 18:26:53 +0200

    1.0.x: Bump versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,14 +15,14 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val agreementManagementVersion         = "1.0.x-SNAPSHOT"
-  lazy val authorizationManagementVersion     = "1.0.x-SNAPSHOT"
-  lazy val attributeRegistryManagementVersion = "1.0.x-SNAPSHOT"
-  lazy val catalogManagementVersion           = "1.0.x-SNAPSHOT"
-  lazy val commonsVersion                     = "1.0.x-SNAPSHOT"
-  lazy val partyManagementVersion             = "1.0.x-SNAPSHOT"
-  lazy val purposeManagementVersion           = "1.0.x-SNAPSHOT"
-  lazy val notifierVersion                    = "1.0.x-SNAPSHOT"
-  lazy val tenantProcessVersion               = "1.0.x-SNAPSHOT"
-  lazy val tenantManagementVersion            = "1.0.x-SNAPSHOT"
+  lazy val agreementManagementVersion         = "1.0.3"
+  lazy val authorizationManagementVersion     = "1.0.3"
+  lazy val attributeRegistryManagementVersion = "1.0.3"
+  lazy val catalogManagementVersion           = "1.0.4"
+  lazy val commonsVersion                     = "1.0.7"
+  lazy val partyManagementVersion             = "1.0.0"
+  lazy val purposeManagementVersion           = "1.0.3"
+  lazy val notifierVersion                    = "1.0.3"
+  lazy val tenantProcessVersion               = "1.0.0"
+  lazy val tenantManagementVersion            = "1.0.0"
 }
