8e5c85e472c12932c234ba05648665e0ecef12f6
Scala Steward <scala-steward@pagopa.it>
2022-10-01 06:47:35 +0000

    Update scalatest to 3.2.14 in 1.0.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,7 +11,7 @@ object Versions {
   lazy val mockitoVersion   = "3.9.0"
   lazy val mustacheVersion  = "0.9.10"
   lazy val scalaMockVersion = "5.2.0"
-  lazy val scalatestVersion = "3.2.12"
+  lazy val scalatestVersion = "3.2.14"
 }
 
 object PagopaVersions {
