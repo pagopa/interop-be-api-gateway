9be07ac7be79cd23b2b67dbe2701b6284cd6aea0
Antonio Gelameris <toniogela89@gmail.com>
2022-10-07 17:42:00 +0200

    Update dependencies (#108)
    * Update sbt-tpolecat to 0.4.1 in 1.0.x
    
    * Update scalafmt-core to 3.5.9 in 1.0.x
    
    * Update sbt-native-packager to 1.9.11 in 1.0.x
    
    * Update sbt-scoverage to 2.0.4 in 1.0.x
    
    * Update scalatest to 3.2.14 in 1.0.x
    
    * Update logback-classic to 1.4.3 in 1.0.x
    
    * Update sbt to 1.7.2 in 1.0.x
    
    Co-authored-by: Scala Steward <scala-steward@pagopa.it>

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,11 +7,11 @@ object Versions {
   lazy val catsVersion              = "2.8.0"
   lazy val jacksonVersion   = "2.11.4" // This cannot be updated yet because akka-serialization use 2.11.x version
   lazy val json4sVersion    = "4.0.4"
-  lazy val logbackVersion   = "1.2.11"
+  lazy val logbackVersion   = "1.4.3"
   lazy val mockitoVersion   = "3.9.0"
   lazy val mustacheVersion  = "0.9.10"
   lazy val scalaMockVersion = "5.2.0"
-  lazy val scalatestVersion = "3.2.12"
+  lazy val scalatestVersion = "3.2.14"
 }
 
 object PagopaVersions {
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,10 +1,10 @@
 addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.11.0")
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.0")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.4")
 
-addSbtPlugin("com.github.sbt" % "sbt-native-packager" % "1.9.9")
+addSbtPlugin("com.github.sbt" % "sbt-native-packager" % "1.9.11")
 
-addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.3.3")
+addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.4.1")
 
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
 
