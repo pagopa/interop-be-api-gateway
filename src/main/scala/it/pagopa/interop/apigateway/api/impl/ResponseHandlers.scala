e7c7015205d6b8e84f3ae2a8c280ec7c5bcade58
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-12-20 17:30:29 +0100

    pin-2210: Fix formatting

--- src/main/scala/it/pagopa/interop/apigateway/api/impl/ResponseHandlers.scala
+++ src/main/scala/it/pagopa/interop/apigateway/api/impl/ResponseHandlers.scala
@@ -163,9 +163,9 @@ object ResponseHandlers extends AkkaResponses {
   )(result: Try[T])(implicit contexts: Seq[(String, String)], logger: LoggerTakingImplicit[ContextFieldsToLog]): Route =
     result match {
       case Success(s)  => success(s)
-      case Failure(ex) =>
-        internalServerError(ex, logMessage)
+      case Failure(ex) => internalServerError(ex, logMessage)
     }
+
   def upsertTenantResponse[T](logMessage: String)(
     success: T => Route
   )(result: Try[T])(implicit contexts: Seq[(String, String)], logger: LoggerTakingImplicit[ContextFieldsToLog]): Route =
