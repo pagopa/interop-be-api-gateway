c7c959ec7bd585a39a39badfdfc328e0dd5a1482
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-12-27 16:33:13 +0100

    pin-2551: Update commons with correlation id in error response

--- src/main/scala/it/pagopa/interop/apigateway/api/impl/package.scala
+++ src/main/scala/it/pagopa/interop/apigateway/api/impl/package.scala
@@ -50,7 +50,7 @@ import scala.util.{Failure, Success, Try}
 package object impl extends SprayJsonSupport with DefaultJsonProtocol {
 
   implicit val problemErrorFormat: RootJsonFormat[ProblemError] = jsonFormat2(ProblemError)
-  implicit val problemFormat: RootJsonFormat[Problem]           = jsonFormat5(Problem)
+  implicit val problemFormat: RootJsonFormat[Problem]           = jsonFormat6(Problem)
 
   implicit val attributeSeedFormat: RootJsonFormat[AttributeSeed] = jsonFormat3(AttributeSeed)
 
