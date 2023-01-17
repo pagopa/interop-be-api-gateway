5f0cc1f5007e83e11f4037bedb7533c8c8638a3e
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-01-17 15:56:08 +0100

    Revert "bump versions"
    This reverts commit 07c0dae703d20a0406288452f721a05ee6a11300.

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,14 +15,14 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val agreementManagementVersion         = "1.0.5"
-  lazy val authorizationManagementVersion     = "1.0.8"
-  lazy val attributeRegistryManagementVersion = "1.0.7"
-  lazy val catalogManagementVersion           = "1.0.9"
-  lazy val commonsVersion                     = "1.0.12"
-  lazy val partyRegistryProxyVersion          = "1.0.4"
-  lazy val purposeManagementVersion           = "1.0.5"
-  lazy val notifierVersion                    = "1.0.6"
-  lazy val tenantProcessVersion               = "1.0.2"
-  lazy val tenantManagementVersion            = "1.0.3"
+  lazy val agreementManagementVersion         = "1.0.x-SNAPSHOT"
+  lazy val authorizationManagementVersion     = "1.0.x-SNAPSHOT"
+  lazy val attributeRegistryManagementVersion = "1.0.x-SNAPSHOT"
+  lazy val catalogManagementVersion           = "1.0.x-SNAPSHOT"
+  lazy val commonsVersion                     = "1.0.x-SNAPSHOT"
+  lazy val partyRegistryProxyVersion          = "1.0.x-SNAPSHOT"
+  lazy val purposeManagementVersion           = "1.0.x-SNAPSHOT"
+  lazy val notifierVersion                    = "1.0.x-SNAPSHOT"
+  lazy val tenantProcessVersion               = "1.0.x-SNAPSHOT"
+  lazy val tenantManagementVersion            = "1.0.x-SNAPSHOT"
 }
