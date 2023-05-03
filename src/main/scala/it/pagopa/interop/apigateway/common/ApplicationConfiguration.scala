f54125d91f169e89e30ab1734e943b09fec39034
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-05-03 20:57:42 +0200

    fix configuration

--- src/main/scala/it/pagopa/interop/apigateway/common/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/apigateway/common/ApplicationConfiguration.scala
@@ -11,34 +11,34 @@ object ApplicationConfiguration {
 
   val config: Config = ConfigFactory.load()
 
-  val serverPort: Int = config.getInt("interop-api-gateway.port")
+  val serverPort: Int = config.getInt("api-gateway.port")
 
-  val agreementManagementURL: String         = config.getString("interop-api-gateway.services.agreement-management")
-  val authorizationManagementURL: String     = config.getString("interop-api-gateway.services.authorization-management")
-  val catalogManagementURL: String           = config.getString("interop-api-gateway.services.catalog-management")
+  val agreementManagementURL: String         = config.getString("api-gateway.services.agreement-management")
+  val authorizationManagementURL: String     = config.getString("api-gateway.services.authorization-management")
+  val catalogManagementURL: String           = config.getString("api-gateway.services.catalog-management")
   val attributeRegistryManagementURL: String =
-    config.getString("interop-api-gateway.services.attribute-registry-management")
-  val partyRegistryProxyURL: String          = config.getString("interop-api-gateway.services.party-registry-proxy")
-  val purposeManagementURL: String           = config.getString("interop-api-gateway.services.purpose-management")
-  val tenantProcessURL: String               = config.getString("interop-api-gateway.services.tenant-process")
-  val tenantManagementURL: String            = config.getString("interop-api-gateway.services.tenant-management")
+    config.getString("api-gateway.services.attribute-registry-management")
+  val partyRegistryProxyURL: String          = config.getString("api-gateway.services.party-registry-proxy")
+  val purposeManagementURL: String           = config.getString("api-gateway.services.purpose-management")
+  val tenantProcessURL: String               = config.getString("api-gateway.services.tenant-process")
+  val tenantManagementURL: String            = config.getString("api-gateway.services.tenant-management")
 
-  val notifierURL: String = config.getString("interop-api-gateway.services.notifier")
+  val notifierURL: String = config.getString("api-gateway.services.notifier")
 
   val jwtAudience: Set[String] =
-    config.getString("interop-api-gateway.jwt.audience").split(",").toSet.filter(_.nonEmpty)
+    config.getString("api-gateway.jwt.audience").split(",").toSet.filter(_.nonEmpty)
 
   val rateLimiterConfigs: LimiterConfig = {
-    val rateInterval = config.getDuration("interop-api-gateway.rate-limiter.rate-interval")
-    val timeout      = config.getDuration("interop-api-gateway.rate-limiter.timeout")
+    val rateInterval = config.getDuration("api-gateway.rate-limiter.rate-interval")
+    val timeout      = config.getDuration("api-gateway.rate-limiter.timeout")
 
     LimiterConfig(
-      limiterGroup = config.getString("interop-api-gateway.rate-limiter.limiter-group"),
-      maxRequests = config.getInt("interop-api-gateway.rate-limiter.max-requests"),
-      burstPercentage = config.getDouble("interop-api-gateway.rate-limiter.burst-percentage"),
+      limiterGroup = config.getString("api-gateway.rate-limiter.limiter-group"),
+      maxRequests = config.getInt("api-gateway.rate-limiter.max-requests"),
+      burstPercentage = config.getDouble("api-gateway.rate-limiter.burst-percentage"),
       rateInterval = FiniteDuration(rateInterval.toMillis, TimeUnit.MILLISECONDS),
-      redisHost = config.getString("interop-api-gateway.rate-limiter.redis-host"),
-      redisPort = config.getInt("interop-api-gateway.rate-limiter.redis-port"),
+      redisHost = config.getString("api-gateway.rate-limiter.redis-host"),
+      redisPort = config.getInt("api-gateway.rate-limiter.redis-port"),
       timeout = FiniteDuration(timeout.toMillis, TimeUnit.MILLISECONDS)
     )
   }
