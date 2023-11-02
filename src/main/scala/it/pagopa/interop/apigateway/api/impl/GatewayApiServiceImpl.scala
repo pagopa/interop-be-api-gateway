4c9d1c2c353b52e96653e48cabfb909589b15b4f
AsterITA <fabrizio.musella@gmail.com>
2023-11-02 09:28:49 +0100

    PIN-4122: added '/purposes/eservices/{eserviceId}/consumer/{consumerId}' (#159)

--- src/main/scala/it/pagopa/interop/apigateway/api/impl/GatewayApiServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/apigateway/api/impl/GatewayApiServiceImpl.scala
@@ -573,4 +573,24 @@ final case class GatewayApiServiceImpl(
       getEservicesResponse[CatalogEServices](operationLabel)(getEServices200)
     }
   }
+
+  override def getPurposes(eserviceId: String, consumerId: String)(implicit
+    contexts: Seq[(String, String)],
+    toEntityMarshallerPurposes: ToEntityMarshaller[Purposes],
+    toEntityMarshallerProblem: ToEntityMarshaller[Problem]
+  ): Route = {
+    val operationLabel = s"Retrieving Purposes for eservice $eserviceId and consumer $consumerId"
+    logger.info(operationLabel)
+
+    val result: Future[Purposes] = for {
+      eserviceUUID   <- eserviceId.toFutureUUID
+      consumerUUID   <- consumerId.toFutureUUID
+      clientPurposes <- purposeProcessService.getAllPurposes(eserviceUUID, consumerUUID)
+      purposes       <- clientPurposes.traverse(_.toModel).toFuture
+    } yield Purposes(purposes)
+
+    onComplete(result) {
+      getPurposesResponse(operationLabel)(getPurposes200, Purposes(purposes = Seq.empty))
+    }
+  }
 }
